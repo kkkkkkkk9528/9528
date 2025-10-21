d01a494a702563ec57d0e70fdd5837b61104cc63
kkkkkkkk9528 <elupe6303@gmail.com>
2025-10-21 19:14:02 +0800

    2025.10.21

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -2,21 +2,26 @@
 pragma solidity ^0.8.20;
 
 import {BEP20Token} from "./token.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title 确定性 CREATE2 工厂
 /// @notice 地址公式：address = keccak256(0xff, factory, salt, keccak256(creationCode+abi.encode(args)))[12:]
-contract Factory {
+contract Factory is Ownable(msg.sender) {
   event TokenDeployed(address indexed addr, bytes32 indexed salt);
 
-  /// @dev 通过 CREATE2 的 new{salt} 
+  /// @dev 通过 CREATE2 的 new{salt}
   function deployToken(
     bytes32 salt,
     string memory name,
     string memory symbol,
     uint256 supply,
     uint8 decimals
-  ) external payable returns (address addr) {
+  ) external payable onlyOwner returns (address addr) {
     BEP20Token tok = new BEP20Token{salt: salt}(name, symbol, supply, decimals);
+
+    // 立即把 owner 转给部署人（msg.sender）
+    tok.transferOwnership(msg.sender);
+
     addr = address(tok);
     emit TokenDeployed(addr, salt);
   }
@@ -31,7 +36,7 @@ contract Factory {
   ) external view returns (address) {
     bytes memory init = abi.encodePacked(type(BEP20Token).creationCode, abi.encode(name, symbol, supply, decimals));
     bytes32 initHash = keccak256(init);
-    return address(uint160(uint(keccak256(abi.encodePacked(bytes1(0xff), address(this), salt, initHash))))); 
+    return address(uint160(uint(keccak256(abi.encodePacked(bytes1(0xff), address(this), salt, initHash)))));
   }
 
   /// @notice 仅返回 initCodeHash，便于前端/脚本与本地计算比对
